971aa313540b4142ed706592081f871ef399de79
VallentinaStef <stefhanievallentina@gmail.com>
2026-03-29 11:05:48 +0700

    menambahkan class HaloGeneric

---
+++ HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MODUL5_103022400002
+{
+    internal class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine("Halo user " + user);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TP_MODUL5_103022400002;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Stefhanie Valentina");
+    }
+}
